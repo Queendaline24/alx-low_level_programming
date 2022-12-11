a3196d0ecf1900cd19ea8f97af0703ccd3ec2b2f
Queendaline24 <queendaline99@icloud.com>
2022-12-11 10:22:10 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
  #include <stdio.h>
+
 /**
  * main - Entry
  * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammer,but the outcome is a piece of art,\n");
-	return (0);
+	puts("\"programming is like building a multi lingual puzzle") ;
+	return (0) ;
 }
