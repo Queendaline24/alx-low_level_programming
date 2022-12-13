801cba4045c2420ff6cbf895296f8740ac798d88
Queendaline24 <queendaline99@icloud.com>
2022-12-12 16:54:07 -0800

    the more brain use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of the digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		{
+			if (!((ones == tens) || (tens > ones)))/* eliminate repitition*/
+
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/* adds comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+				
+
+
