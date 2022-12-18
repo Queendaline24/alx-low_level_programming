726f04722fb7e1dab4451f3df7c0a810bbd30aed
Queendaline24 <queendaline99@icloud.com>
2022-12-18 07:54:08 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,14 @@
  * 10 times
  * Return: no return
  */
+
 void more_numbers(void)
 {
 	int i, ch;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (ch = 0, ch < 15; ch++)
+		for (ch = 0; ch < 15; ch++)
 		{
 			if (ch >= 10)
 				_putchar((ch / 10) + 48);
