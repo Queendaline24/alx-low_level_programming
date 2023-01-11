6e0ecd1bc801d54a8b2963812b5f689ce4c80497
Queendaline24 <queendaline99@icloud.com>
2023-01-10 17:48:13 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i <, height; i++)
+	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *) malloc(sizeof(int) * width);
 		if (array[i] == NULL)
