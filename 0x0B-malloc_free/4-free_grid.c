3567af8eabce067c89dd792a9d7107dbf5a8ca5c
Queendaline24 <queendaline99@icloud.com>
2023-01-10 18:02:42 -0800

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * free_grid - frees the memory allocate for the grid
- * created by alloc_grid()
+ * created by alloc_grid
  * @grid: grid to free
  * @height: height of the grid
  */
