6fb3b5a3aade00c94b64852fd2115e1477ff144e
Queendaline24 <queendaline99@icloud.com>
2023-01-11 18:24:18 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,7 +6,7 @@
  * main - adds positive numbers.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
- * Return: 0 - success
+ * Return: 0 - Success
  */
 int main(int argc, char *argv[])
 {
