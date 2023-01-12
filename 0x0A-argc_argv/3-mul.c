6d72e6acfffbcb1fdb9fcfb19958e43c0f513127
Queendaline24 <queendaline99@icloud.com>
2023-01-11 17:53:34 -0800

    4-add.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,64 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints the multiplications of two integers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if true, 1 if false
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ *
  */
-int main(int argc, char *argv[])
+int _atoi(char *s)
 {
-	int a, b;
+	int i, d, n, len, f, digit;
 
-	if (argc == 1)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+
+	digit = 0;
+
+	while (s[len] != '\0')
+
+		len++;
+
+	while (i < len && f == 0)
+
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-		return (0);
+		if (s[i] == '-')
+			
+			++d,
+
+				if (s[i] >= '0' && s[i] <= '9')
+
+				{
+					digit = s[i] - '0';
+
+					if (d % 2)
+
+						digit = digit;
+
+					n = n * 10 + digit;
+
+					f = 1;
+
+					if (s[i + 1] < '0' || s[i + 1] > '9')
+
+						break;
+
+					f = 0;
+
+				}
+
+		i++;
+
 	}
-	printf("Error\n");
-	return (1);
+
+	if (f == 0)
+
+		return (0);
+
+	return (n);
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds positive number.
+ * @argc: number of command line argument
+ * @argv: array that contains the program command line argument
+ * Return: 0 - suuccess
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, and = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n")
+					return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	print("%d\n", add);
+	return (0);
+}
+
