563415d123877118d067e2afef3e2bfcf5c928fe
Queendaline24 <queendaline99@icloud.com>
2023-01-13 17:47:08 -0800

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector
+ *
+ * Return: no return
+ */
+void _is_zero(char *argv[])
+{
+	int i, isn1 = 1, isn2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
+}
