8f5804890c222e1386da6aa7867f4cbd1cd4d4b6
Queendaline24 <queendaline99@icloud.com>
2023-01-17 17:47:35 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-typedef struct dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
